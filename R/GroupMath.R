7689ca3bd3df22cab92dcf689ec489757884d995
Roman Pahl <roman.pahl@gmail.com>
2021-03-07 13:18:53 +0100

    Revise group math operators

--- R/GroupMath.R
+++ R/GroupMath.R
@@ -1,24 +1,24 @@
 #' @export
 Math.Container <- function(x, ...)
 {
-    as.container(get(.Generic)(unlist(x$values()), ...))
+    as.container(get(.Generic)(unpack(x), ...))
 }
 
 #' @export
 Math.Deque <- function(x, ...)
 {
-    as.deque(get(.Generic)(unlist(x$values()), ...))
+    as.deque(get(.Generic)(unpack(x), ...))
 }
 
 #' @export
 Math.Dict <- function(x, ...)
 {
-    as.dict(get(.Generic)(unlist(x$values()), ...))
+    as.dict(get(.Generic)(unpack(x), ...))
 }
 
 #' @export
 Math.Set <- function(x, ...)
 {
-    as.set(get(.Generic)(x$values(), ...))
+    as.set(get(.Generic)(unpack(x), ...))
 }
 
--- inst/tinytest/test_GroupMath.R
+++ inst/tinytest/test_GroupMath.R
@@ -7,6 +7,14 @@ ee(exp(log(container(1, 2))), container(1, 2))
 ee(cumsum(container(1, 2, 3)), container(1, 3, 6))
 ee(cumsum(container(3, 2, 1)), container(3, 5, 6))
 
+ee(exp(container(1, 2, container(3, 4))), as.container(exp(1:4)))
+ee(exp(container(1, 2, list(3, 4))), as.container(exp(1:4)))
+ee(exp(container(1, 2, list(container(3), 4))), as.container(exp(1:4)))
+ee(exp(container(1, x = 2)), as.container(exp(c(1, x = 2))))
+
+ee(cumsum(container(two = 2, one = 1, co = container(one = 1, two = 2))),
+          container(two = 2, one = 3, co.one = 4,          co.two = 6))
+
 # -----
 # Deque
 # -----
@@ -15,6 +23,10 @@ ee(abs(deque(-1, 1)), deque(1, 1))
 ee(exp(log(deque(1, 2))), deque(1, 2))
 ee(cumsum(deque(1, 2, 3)), deque(1, 3, 6))
 ee(cumsum(deque(3, 2, 1)), deque(3, 5, 6))
+
+ee(exp(deque(1, 2, deque(3, 4))), as.deque(exp(1:4)))
+ee(exp(deque(1, 2, list(3, 4))), as.deque(exp(1:4)))
+ee(exp(deque(1, 2, list(deque(3), 4))), as.deque(exp(1:4)))
 
 # ----
 # Dict
@@ -28,6 +40,14 @@ ee(cumsum(dict(a = 1, b = 2, c = 3)),
 ee(cumsum(dict(c = 2, b = 1, a = 3)),
           dict(a = 3, b = 4, c = 6)) # first sorted then calculated
 
+ee(abs(dict(a = -1, b = 2, d = dict(a = -3))),
+       dict(a =  1, b = 2, d.a = 3))
+ee(abs(dict(a = -1, b = 2, d = dict(x = -3))),
+       dict(a =  1, b = 2, d.x = 3))
+
+expect_error(abs(dict(d.a = -1, b = 2, d = dict(a = -3))), "duplicated keys")
+
+
 # ---
 # Set
 # ---
@@ -37,4 +57,10 @@ ee(exp(log(setnew(1, 2))), setnew(1, 2))
 ee(cumsum(setnew(1, 2, 3)), setnew(1, 3, 6))
 ee(cumsum(setnew(3, 2, 1)),
           setnew(1, 3, 6)) # first sorted then calculated
+
+ee(cumsum(setnew(two = 2, one = 1, s = setnew(one = 1, two = 2))),
+          setnew(one = 1, two = 3, s.one = 4,        s.two = 6))
+
+ee(cumsum(setnew(two = 2, one = 1, s = setnew(1, one = 1, two = 2))),
+          setnew(one = 1, two = 3, s1 = 4,              s.two = 6))
 
