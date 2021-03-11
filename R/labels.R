4372bc7e80d9357328242e4b12867b32ff3fa6d8
Roman Pahl <roman.pahl@gmail.com>
2021-03-11 20:38:44 +0100

    Simplify and fix LABELS function

--- R/labels.R
+++ R/labels.R
@@ -1,43 +1,42 @@
-# Most of the code below is borrowed from the 'sets' package and extended by
-# some LABEL.x functions for Container and other classes.
+# Most of the code below is borrowed from the 'sets' package with some
+# minor adjustments and fixes and extended by some functions to handle
+# Container and other classes.
 LABELS <-
-function(x, max_width = NULL, dots = "...", unique = FALSE, limit = NULL, ...)
+function(x, limit = NULL, ...)
 {
     x <- as.list(x)
-    l <- length(x)
-
-    ## recycle max_width and dots as needed
-    if (!is.null(max_width))
-        max_width <- rep_len(max_width, length.out = l)
-    dots <- rep_len(dots, length.out = l)
+    len <- length(x)
 
     ## check existing labels
     ret <- names(x)
     if (is.null(ret))
-        ret <- rep.int("", l)
+        ret <- rep.int("", len)
 
     ## create a label for components without given one
     empty <- is.na(ret) | (ret == "")
     if (any(empty))
         ret[empty] <- sapply(x[empty], LABEL, limit, ...)
 
-    ## check maximum width (max_width == NULL => unbounded)
-    if (!is.null(max_width)) {
-        too_long <- nchar(ret, "width") > max_width
-        if (any(too_long)) {
-            ret[too_long] <- strtrim(ret[too_long], max_width[too_long])
-
-            ## possibly add dots
-            if (!is.null(dots))
-                ret[too_long] <- paste0(ret[too_long], dots[too_long])
-          }
-    }
-
-    if (unique)
-      ret <- make.unique(ret)
-
     ret
 }
+
+
+.format_or_class <-
+function(x, limit, ...)
+{
+    l <- length(x)
+    if (l == 0)
+        return(paste0(class(x)[1L], "()"))
+
+    if (l >= limit)
+        return(paste0("<<", class(x)[1L], "(", l, ")>>"))
+
+    if (is.integer(x))
+        format(ifelse(is.na(x), x, paste0(x, "L")), ...)
+    else
+        format(x, ...)
+}
+
 
 #' @export
 LABEL <- function(x, limit = NULL, ...) UseMethod("LABEL")
@@ -57,7 +56,10 @@ LABEL.numeric <-
 function(x, limit = NULL, ...) {
     if (is.null(limit))
         limit <- 2L
-    .format_or_class(x, limit, ...)
+    s = .format_or_class(x, limit, ...)
+    if (length(s) > 1)
+        s = paste0("(", toString(s), ")")
+    s
 }
 
 #' @export
@@ -74,9 +76,16 @@ LABEL.character <-
 function(x, limit = NULL, quote = TRUE, ...) {
     if (is.null(limit))
         limit <- 2L
-    if (quote)
+
+    s = if (quote)
         x <- ifelse(is.na(x), x, paste0("\"", x, "\""))
+
     .format_or_class(x, limit, ...)
+
+    if (length(s) > 1)
+        s = paste0("(", toString(s), ")")
+    s
+
 }
 
 #' @export
@@ -85,42 +94,6 @@ function(x, limit = NULL, ...) {
     if (is.null(limit))
         limit <- 1L
     .format_or_class(x, limit, ...)
-}
-
-#' @export
-LABEL.set <-
-function(x, limit = NULL, ...) {
-    if (is.null(limit))
-        limit <- 6L
-    .format_or_class(x, limit, ...)
-}
-
-#' @export
-LABEL.gset <- LABEL.set
-
-#' @export
-LABEL.cset <- LABEL.set
-
-#' @export
-LABEL.tuple <- LABEL.set
-
-#' @export
-LABEL.interval <- LABEL.set
-
-.format_or_class <-
-function(x, limit, ...)
-{
-    l <- length(x)
-    if (l == 0)
-        return(paste0(class(x)[1L], "()"))
-
-    if (l >= limit)
-        return(paste0("<<", class(x)[1L], "(", l, ")>>"))
-
-    if (is.integer(x))
-        format(ifelse(is.na(x), x, paste0(x, "L")), ...)
-    else
-        format(x, ...)
 }
 
 
