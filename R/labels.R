8e4e4ef329ac25b14b14e003872c0dc8f1e8e9b0
Roman Pahl <roman.pahl@gmail.com>
2021-03-11 22:27:10 +0100

    Fix labels

--- R/labels.R
+++ R/labels.R
@@ -1,4 +1,4 @@
-# Most of the code below is borrowed from the 'sets' package with some
+# The basic code idea below is borrowed from the 'sets' package with some
 # minor adjustments and fixes and extended by some functions to handle
 # Container and other classes.
 LABELS <-
@@ -7,55 +7,39 @@ function(x, limit = NULL, ...)
     x <- as.list(x)
     len <- length(x)
 
-    ## check existing labels
-    ret <- names(x)
-    if (is.null(ret))
-        ret <- rep.int("", len)
+    # Where names are defined, we use them as labels
+    labels <- names(x)
+    if (is.null(labels))
+        labels <- rep.int("", len)
 
-    ## create a label for components without given one
-    empty <- is.na(ret) | (ret == "")
-    if (any(empty))
-        ret[empty] <- sapply(x[empty], LABEL, limit, ...)
+    noLabel <- is.na(labels) | (labels == "")
+    if (any(noLabel))
+        labels[noLabel] <- sapply(x[noLabel], LABEL, limit, ...)
 
-    ret
-}
-
-
-.format_or_class <-
-function(x, limit, ...)
-{
-    l <- length(x)
-    if (l == 0)
-        return(paste0(class(x)[1L], "()"))
-
-    if (l >= limit)
-        return(paste0("<<", class(x)[1L], "(", l, ")>>"))
-
-    if (is.integer(x))
-        format(ifelse(is.na(x), x, paste0(x, "L")), ...)
-    else
-        format(x, ...)
+    labels
 }
 
 
 #' @export
 LABEL <- function(x, limit = NULL, ...) UseMethod("LABEL")
 
-#' @export
-LABEL.default <- function(x, limit = NULL, ...) {
-    paste0("<<", class(x)[1L], ">>")
-}
 
 #' @export
-LABEL.matrix <- function(x, limit = NULL, ...) {
+LABEL.default <- function(x, limit = NULL, ...)
+    paste0("<<", class(x)[1L], ">>")
+
+
+#' @export
+LABEL.matrix <- function(x, limit = NULL, ...)
     sprintf("<<%ix%i matrix>>", nrow(x), ncol(x))
-}
 
 #' @export
 LABEL.numeric <-
-function(x, limit = NULL, ...) {
+function(x, limit = NULL, ...)
+{
     if (is.null(limit))
         limit <- 2L
+
     s = .format_or_class(x, limit, ...)
     if (length(s) > 1)
         s = paste0("(", toString(s), ")")
@@ -73,14 +57,15 @@ LABEL.logical <- LABEL.numeric
 
 #' @export
 LABEL.character <-
-function(x, limit = NULL, quote = TRUE, ...) {
+function(x, limit = NULL, quote = TRUE, ...)
+{
     if (is.null(limit))
         limit <- 2L
 
-    s = if (quote)
+    if (quote)
         x <- ifelse(is.na(x), x, paste0("\"", x, "\""))
 
-    .format_or_class(x, limit, ...)
+    s = .format_or_class(x, limit, ...)
 
     if (length(s) > 1)
         s = paste0("(", toString(s), ")")
@@ -89,11 +74,20 @@ function(x, limit = NULL, quote = TRUE, ...) {
 }
 
 #' @export
-LABEL.list <-
-function(x, limit = NULL, ...) {
+LABEL.list <- function(x, limit = NULL, ...)
+{
     if (is.null(limit))
-        limit <- 1L
-    .format_or_class(x, limit, ...)
+        limit <- 0L
+
+    s = .format_or_class(x, limit, ...)
+
+    if (length(s) > 1)
+        s = paste0("list(", toString(s), ")")
+
+    if (length(s) == 1 && !startsWith(s, "list") && !startsWith(s, "<"))
+        s = paste0("list(", toString(s), ")")
+
+    s
 }
 
 
@@ -101,25 +95,39 @@ function(x, limit = NULL, ...) {
 LABEL.Container <- function(x, limit = NULL, ...)
 {
     if (is.null(limit))
-        limit <- 6L
+        limit <- 5L
     .format_or_class(x, limit, ...)
 }
 
 #' @export
 LABEL.data.frame <- function(x, limit = NULL, ...)
-{
     sprintf("<<%s(%ix%i)>>", data.class(x), nrow(x), ncol(x))
-}
+
 
 #' @export
 LABEL.data.table <- function(x, limit = NULL, ...)
-{
     sprintf("<<%ix%i data.table>>", nrow(x), ncol(x))
-}
+
 
 #' @export
 LABEL.dict.table <- function(x, limit = NULL, ...)
-{
     sprintf("<<dict.table(%ix%i)>>", nrow(x), ncol(x))
+
+
+.format_or_class <-
+function(x, limit, ...)
+{
+    l <- length(x)
+    if (l == 0)
+        return(paste0(class(x)[1L], "()"))
+
+    if (l > limit)
+        return(paste0("<<", class(x)[1L], "(", l, ")>>"))
+
+    if (is.integer(x))
+        format(ifelse(is.na(x), x, paste0(x, "L")), ...)
+    else
+        format(x, ...)
 }
+
 
--- inst/tinytest/test_format.R
+++ inst/tinytest/test_format.R
@@ -2,39 +2,57 @@
 # format of values
 # ----------------
 ee = expect_equal
-f = .format_values
+f = function(...) .format_values(..., left = "", right ="")
 
-ee(f(list(a = 1)), "(a = 1)")
-ee(f(list(container(1))), "([1])")
-ee(f(list(as.container(1:5))), "([1L, 2L, 3L, 4L, 5L])")
-ee(f(list(as.container(1:6))), "(<<Container(6)>>)")
-ee(f(list(as.container(1:10))), "(<<Container(10)>>)")
-ee(f(list(as.container(1:10)), limit = 20), "([1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L])")
+ee(f(list(a = 1)), "a = 1")
+ee(f(list(container(1))), "[1]")
+ee(f(list(as.container(1:5))), "[1L, 2L, 3L, 4L, 5L]")
+ee(f(list(as.container(1:6))), "<<Container(6)>>")
+ee(f(list(as.container(1:10))), "<<Container(10)>>")
+ee(f(list(as.container(1:10)), limit = 20), "[1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L]")
 
-ee(f(list(deque(1))), "(|1|)")
-ee(f(list(as.deque(1:5))), "(|1L, 2L, 3L, 4L, 5L|)")
-ee(f(list(as.deque(1:6))), "(<<Deque(6)>>)")
+ee(f(list(deque(1))), "|1|")
+ee(f(list(as.deque(1:5))), "|1L, 2L, 3L, 4L, 5L|")
+ee(f(list(as.deque(1:6))), "<<Deque(6)>>")
 
-ee(f(list(setnew(1))), "({1})")
-ee(f(list(as.set(1:5))), "({1L, 2L, 3L, 4L, 5L})")
-ee(f(list(as.set(1:6))), "(<<Set(6)>>)")
+ee(f(list(setnew(1))), "{1}")
+ee(f(list(as.set(1:5))), "{1L, 2L, 3L, 4L, 5L}")
+ee(f(list(as.set(1:6))), "<<Set(6)>>")
 
-ee(f(list(dict(a = 1))), "({a = 1})")
+ee(f(list(dict(a = 1))), "{a = 1}")
 v = 1:10
 names(v) = letters[1:10]
-ee(f(list(as.dict(v[1:5]))), "({a = 1L, b = 2L, c = 3L, d = 4L, e = 5L})")
-ee(f(list(as.dict(v[1:6]))), "(<<Dict(6)>>)")
+ee(f(list(as.dict(v[1:5]))), "{a = 1L, b = 2L, c = 3L, d = 4L, e = 5L}")
+ee(f(list(as.dict(v[1:6]))), "<<Dict(6)>>")
 
+ee(f(container()), "")
+ee(f(container(list( ))), "list()")
+ee(f(container(list(1))), "<<list(1)>>")
+ee(f(container(list(1)), limit = 1), "list(1)")
+ee(f(container(list(1, 2)), limit = 1), "<<list(2)>>")
+ee(f(container(list(1, 2)), limit = 2), "list(1, 2)")
+
+ee(f(container(numeric( ))), "numeric()")
+ee(f(container(numeric(0))), "numeric()")
+ee(f(container(c(1))), "1")
+ee(f(container(c(1)), limit = 1), "1")
+ee(f(container(c(1, 2)), limit = 1), "<<numeric(2)>>")
+ee(f(container(c(1, 2)), limit = 2), "(1, 2)")
+
+ee(f(container(c("a"))), '"a"')
+ee(f(container(c("a")), limit = 1), '"a"')
+ee(f(container(c("a", "b")), limit = 1), "<<character(2)>>")
+ee(f(container(c("a", "b")), limit = 2), '("a", "b")')
 
 # -------
 # formats
 # -------
 l = list(1, 2)
-expect_equal(format.Container(l), "[1, 2]")
+ee = expect_equal
+ee(format.Container(1), "[1]")
+ee(format.Deque(1), "|1|")
+ee(format.Dict(1), "{1}")
+ee(format.Set(1), "{1}")
 
-expect_equal(format.Deque(l), "|1, 2|")
 
-expect_equal(format.Dict(l), "{1, 2}")
-
-expect_equal(format.Set(l), "{1, 2}")
 
