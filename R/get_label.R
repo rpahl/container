20e67127f0eb33f7815b6ab4ba5fec108d586efb
Roman Pahl <roman.pahl@gmail.com>
2021-03-13 22:57:02 +0100

    refactor get_label

--- R/get_label.R
+++ R/get_label.R
@@ -5,106 +5,108 @@
 #' @seealso [sets::LABEL()]
 #' @noRd
 #' @export
-get_label <- function(x, vec.len = NULL, ...)
+get_label <-
+function(x, vec.len = 4L, ...)
     UseMethod("get_label")
 
 
 #' @export
-get_label.default <- function(x, ...)
-    paste0("<<", data.class(x), ">>")
+get_label.default <-
+function(x, ...) {
+    dim.x = dim(x)
+    if (is.null(dim.x))
+        dim.x = length(x)
+
+    left = paste0("<<", class(x)[1L], "(")
+    mid = paste(dim.x, collapse = "x")
+    right = ")>>"
+
+    paste0(left, mid, right)
+}
 
 
 .get_atomic_label <-
-function(x, vec.len = NULL, ...)
+function(x, vec.len = 4L, useDots = TRUE, ...)
 {
-    if (is.null(vec.len))
-        vec.len = utils::strOptions()$vec.len
+    s = .format_or_class_label(x, vec.len = ifelse(useDots, Inf, vec.len), ...)
+    s = trimws(s)
 
-    s = .format_or_class_label(x, vec.len, ...)
+    if (length(s) <= 1)
+        return(s)
 
-    if (length(s) > 1)
-        s = paste0("(", paste(s, collapse = " "), ")")
-    s
+    if (length(s) > vec.len)
+        s = s[1:vec.len]
+
+    dots_or_empty = if(useDots && length(x) > vec.len) " ..." else ""
+
+    paste0("(", paste(s, collapse = " "), dots_or_empty, ")")
 }
 
-#' @export
-get_label.numeric <- .get_atomic_label
 
 #' @export
-get_label.factor <- .get_atomic_label
+get_label.numeric <-
+    .get_atomic_label
 
 #' @export
-get_label.integer <- .get_atomic_label
+get_label.factor <-
+    .get_atomic_label
 
 #' @export
-get_label.logical <- .get_atomic_label
+get_label.integer <-
+    .get_atomic_label
+
+#' @export
+get_label.logical <-
+function(x, vec.len = 4L, ...)
+{
+    .get_atomic_label(x, vec.len = max(1L, vec.len / 2), ...)
+}
+
 
 #' @export
 get_label.character <-
-function(x, vec.len = NULL, ...)
-{
-    if (is.null(vec.len))
-        vec.len = utils::strOptions()$vec.len
+function(x, ...) {
+    if (length(x))
+        x <- ifelse(is.na(x), x, paste0("\"", x, "\""))
 
-    x <- ifelse(is.na(x), x, paste0("\"", x, "\""))
-    .get_atomic_label(x)
-}
-
-#' @export
-get_label.list <- function(x, vec.len = NULL, ...)
-{
-    if (is.null(vec.len))
-        vec.len = utils::strOptions()$vec.len
-
-    .format_or_class_label(x, vec.len, ...)
+    .get_atomic_label(x, ...)
 }
 
 
 #' @export
-get_label.Container <- function(x, vec.len = NULL, ...)
-{
-    if (is.null(vec.len))
-        vec.len = utils::strOptions()$vec.len
-
-    .format_or_class_label(x, vec.len, ...)
+get_label.list <-
+function(x, ...) {
+    .format_or_class_label(x, ...)
 }
 
-#' @export
-get_label.matrix <- function(x, ...)
-    sprintf("<<%ix%i matrix>>", nrow(x), ncol(x))
 
 #' @export
-get_label.data.frame <- function(x, ...)
-    sprintf("<<%s(%ix%i)>>", data.class(x), nrow(x), ncol(x))
-
-
-#' @export
-get_label.data.table <- function(x, ...)
-    sprintf("<<%ix%i data.table>>", nrow(x), ncol(x))
+get_label.Container <-
+function(x, ...) {
+    .format_or_class_label(x, ...)
+}
 
 
 #' @export
-get_label.dict.table <- function(x, ...)
-    sprintf("<<dict.table(%ix%i)>>", nrow(x), ncol(x))
+get_label.matrix <-
+function(x, ...)
+    get_label.default(x, ...)
 
 
 
 
-.format_or_class_label <- function(x, vec.len = NULL, ...)
+.format_or_class_label <-
+function(x, vec.len = 4L, markInteger = TRUE, ...)
 {
-    if (is.null(vec.len))
-        vec.len = utils::strOptions()$vec.len
-
     len <- length(x)
     if (len == 0)
-        return(paste0(data.class(x), "()"))
+        return(paste0(class(x)[1L], "()"))
 
     createClassLabel = len > vec.len
     if (createClassLabel)
-        return(paste0("<<", data.class(x), "(", len, ")>>"))
+        return(paste0("<<", class(x)[1L], "(", len, ")>>"))
 
-    isUsingIdentical = TRUE
-    if (is.integer(x) && isUsingIdentical)
+    if (is.integer(x) && markInteger)
         format(ifelse(is.na(x), x, paste0(x, "L")), ...)
     else
         format(x, ...)
--- inst/tinytest/test_get_label.R
+++ inst/tinytest/test_get_label.R
@@ -1,1 +1,100 @@
+f... = get_label
+f = function(...) get_label(..., useDots = FALSE)
+ee = expect_equal
+
+expect_error(f...())
+expect_error(f())
+
+# -------
+# numeric
+# -------
+ee(f...(as.numeric(NA)), "NA")
+ee(f(as.numeric(NA)), "NA")
+ee(f...(numeric()), "numeric()")
+ee(f...(double()), "numeric()")
+ee(f...(numeric(1)), "0")
+ee(f...(numeric(4)), "(0 0 0 0)")
+ee(f...(numeric(5)), "(0 0 0 0 ...)")
+ee(f...(numeric(5), vec.len = 5), "(0 0 0 0 0)")
+ee(f...(numeric(3), vec.len = 1), "(0 ...)")
+ee(f(numeric(4)), f...(numeric(4)))
+ee(f(numeric(5)), "<<numeric(5)>>")
+ee(f(numeric(5), vec.len = 1), "<<numeric(5)>>")
+ee(f...(1), "1")
+ee(f...(1:2/2), "(0.5 1.0)")
+
+
+# ------
+# factor
+# ------
+ee(f...(as.factor(NA)), "NA")
+ee(f(as.factor(NA)), "NA")
+ee(f...(factor()), "factor()")
+ee(f...(factor(1)), "1")
+ee(f...(factor(4)), "4")
+ee(f...(factor(1:4)), "(1 2 3 4)")
+ee(f...(factor(1:5)), "(1 2 3 4 ...)")
+ee(f...(factor(1:5), vec.len = 5), "(1 2 3 4 5)")
+ee(f...(factor(1:3), vec.len = 1), "(1 ...)")
+ee(f(factor(1:4)), f...(factor(1:4)))
+ee(f(factor(1:5)), "<<factor(5)>>")
+ee(f(factor(1:2), vec.len = 1), "<<factor(2)>>")
+
+
+# -------
+# integer
+# -------
+ee(f...(as.integer(NA)), "NA")
+ee(f(as.integer(NA)), "NA")
+ee(f...(integer()), "integer()")
+ee(f...(integer(1)), "0L")
+ee(f...(integer(4)), "(0L 0L 0L 0L)")
+ee(f...(integer(5)), "(0L 0L 0L 0L ...)")
+ee(f...(integer(5), vec.len = 5), "(0L 0L 0L 0L 0L)")
+ee(f...(integer(3), vec.len = 1), "(0L ...)")
+ee(f(integer(4)), f...(integer(4)))
+ee(f(integer(5)), "<<integer(5)>>")
+ee(f(integer(2), vec.len = 1), "<<integer(2)>>")
+ee(f...(1:100), "(1L 2L 3L 4L ...)")
+ee(f(1:100), "<<integer(100)>>")
+ee(f...(1:3, markInteger = FALSE), "(1 2 3)")
+ee(f...(1:10, markInteger = FALSE), "(1 2 3 4 ...)")
+
+
+# -------
+# logical
+# -------
+ee(f...(as.logical(NA)), "NA")
+ee(f(as.logical(NA)), "NA")
+ee(f...(logical()), "logical()")
+ee(f...(logical(1)), "FALSE")
+ee(f...(logical(3)), "(FALSE FALSE ...)")
+ee(f...(logical(3), vec.len = 5), "(FALSE FALSE ...)")
+ee(f...(logical(3), vec.len = 6), "(FALSE FALSE FALSE)")
+ee(f...(logical(4), vec.len = 6), "(FALSE FALSE FALSE ...)")
+
+ee(f...(logical(3), vec.len = 1), "(FALSE ...)")
+ee(f(logical(2)), f...(logical(2)))
+ee(f(logical(5)), "<<logical(5)>>")
+ee(f(logical(2), vec.len = 1), "<<logical(2)>>")
+
+
+# ---------
+# character
+# ---------
+ee(f...(as.character(NA)), "NA")
+ee(f(as.character(NA)), "NA")
+ee(f...(character()), "character()")
+ee(f...(character(1)), '""')
+ee(f...(character(4)), '("" "" "" "")')
+ee(f...(character(5)), '("" "" "" "" ...)')
+ee(f...(character(5), vec.len = 5), '("" "" "" "" "")')
+ee(f...(character(3), vec.len = 1), '("" ...)')
+ee(f(character(4)), f...(character(4)))
+ee(f(character(5)), "<<character(5)>>")
+ee(f(character(2), vec.len = 1), "<<character(2)>>")
+
+
+
+
 
