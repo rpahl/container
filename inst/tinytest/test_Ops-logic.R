ad9a3f146341010dee46d1d791c71e2da8dc207e
Roman Pahl <roman.pahl@gmail.com>
2021-04-03 21:18:18 +0200

    unit tests for key union

--- inst/tinytest/test_Ops-logic.R
+++ inst/tinytest/test_Ops-logic.R
@@ -3,7 +3,8 @@
 # ----
 ee = expect_equal
 
-# &-Operator
+# Key intersection
+# ----------------
 d0   = dict()
 d1   = dict(a = 1)
 d2   = dict(       b = 2)
@@ -41,9 +42,71 @@ d1  = dict(a = 1)
 dd1 = dict(a = 1, d = d1)
 dd2 = dict(b = 1, d = d1)
 res = dd1 & dd2
-ee(res, dict(d = dict(a = 1)))
+d.ref = dict(d = dict(a = 1))
+ee(res, d.ref)
 d1$add("b", 2)
-ee(res, dict(d = dict(a = 1)))
+has_used_copy_semantics <- res == d.ref
+expect_true(has_used_copy_semantics)
+
+
+# dicts combined with other objects
+d = dict(a = 1, b = 2)
+l = list(a = 3, c = 2)
+ee(d & l, dict(a = 1))
+ee(l & d, dict(a = 3))
+ee(dict(b = 1, d = 2) & c(a = 1, b = 2, c = 3, d = 4), dict(b = 1, d = 2))
+
+# Key union
+# ---------
+d0   = dict()
+d1   = dict(a = 1)
+d2   = dict(       b = 2)
+d12  = dict(a = 1, b = 2)
+d23  = dict(       b = 2, c = 3)
+d1_3 = dict(a = 1,        c = 3)
+d123 = dict(a = 1, b = 2, c = 3)
+
+ee(d0 | d0, d0)
+ee(d0 | d1, d1)
+ee(d1 | d0, d1)
+ee(d1 | d1, d1)
+ee(d1 | d12, d12)
+ee(d12 | d1, d12)
+ee(d12 | d23, d123)
+ee(d23 | d12, d123)
+ee(d2 | d1_3, d123)
+ee(d1_3 | d2, d123)
+ee(d12 | d123, d123)
+
+original_dicts_were_not_altered =
+    isTRUE(all.equal(d0,   dict()))                 &&
+    isTRUE(all.equal(d1,   dict(a=1)))              &&
+    isTRUE(all.equal(d2,   dict(     b=2)))         &&
+    isTRUE(all.equal(d12,  dict(a=1, b=2)))         &&
+    isTRUE(all.equal(d23,  dict(     b=2, c=3)))    &&
+    isTRUE(all.equal(d1_3, dict(a=1,      c=3)))    &&
+    isTRUE(all.equal(d123, dict(a=1, b=2, c=3)))
+
+expect_true(original_dicts_were_not_altered)
+
+# Nested dicts
+d1  = dict(a = 1)
+dd1 = dict(a = 1, d = d1)
+dd2 = dict(b = 1, d = d1)
+res = dd1 | dd2
+d.ref = dict(a = 1, b = 1, d = dict(a = 1))
+ee(res, d.ref)
+d1$add("b", 2)
+has_used_copy_semantics <- res == d.ref
+expect_true(has_used_copy_semantics)
+
+
+# dicts combined with other objects
+d = dict(a = 1, b = 2)
+l = list(a = 3, c = 2)
+ee(d & l, dict(a = 1))
+ee(l & d, dict(a = 3))
+ee(dict(b = 1, d = 2) & c(a = 1, b = 2, c = 3, d = 4), dict(b = 1, d = 2))
 
 
 # ---
@@ -90,15 +153,18 @@ s1  = setnew(1)
 ss1 = setnew(1, s1)
 ss2 = setnew(2, s1)
 res = ss1 & ss2
-ee(res, setnew(setnew(1)))
+s.ref = setnew(setnew(1))
+ee(res, s.ref)
 s1$add(2)
-ee(res, setnew(setnew(1)))
+has_used_copy_semantics <- res == s.ref
+expect_true(has_used_copy_semantics)
+
 
 # Named elements
 s1 = setnew(a = 1, b = 2)
 s2 = setnew(a = 2, c = 3)
 ee(s1 & s2, setnew(b = 2))
-ee(s2 & s1, setnew(b = a))
+ee(s2 & s1, setnew(a = 2))
 
 # sets combined with other objects
 ee(s123 & list(1, 2), setnew(1, 2))
