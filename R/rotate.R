30ddbe683a3a2948bbca9e66d5b77de58bca8c42
Roman Pahl <roman.pahl@gmail.com>
2021-04-30 21:39:55 +0200

    Revise rotate, add unit tests and man

--- R/rotate.R
+++ R/rotate.R
@@ -5,11 +5,43 @@
 #' @param x any `R` object.
 #' @param n `integer` number of steps to rotate
 #' @param ... additional arguments to be passed to or from methods.
+#' @details While `rotate` uses copy semantics, `rotate_` works by reference,
+#' that is, rotates in place on the original object.
 #' @export
 rotate <- function(x, ...) UseMethod("rotate")
 
 #' @rdname rotate
-#' @return For `Deque` invisibly returns the [Deque()] object rotated in place.
 #' @export
-rotate.Deque <- function(x, n = 1L) x$rotate(n)
+rotate_ <- function(x, ...) UseMethod("rotate_")
+
+
+#' @rdname rotate
+#' @return For `Deque` returns the rotated [Deque()] object.
+#' @export
+#' @examples
+#' d = deque(1, 2, 3, 4)
+#' rotate(d)
+#' rotate(d, n = 2)
+rotate.Deque <- function(x, n = 1L)
+{
+    (rotate_(x$clone(deep = TRUE), n))
+}
+
+#' @name rotate.Deque
+#' @rdname DequeS3
+#' @usage
+#' rotate(x, n)
+#' @details
+#' * `rotate(x, n)` rotate all elements `n` steps to the right, If `n` is
+#' negative, rotate to the left.
+#' @examples
+#'
+NULL
+
+#' @rdname rotate
+#' @export
+rotate_.Deque <- function(x, n = 1L)
+{
+    invisible(x$rotate(n))
+}
 
