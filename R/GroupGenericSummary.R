5e0ae21fece9f6f69a27eb9174e9e6658a1788f4
Roman Pahl <roman.pahl@gmail.com>
2021-05-13 15:20:14 +0200

    Add examples to summary Ops

--- R/GroupGenericSummary.R
+++ R/GroupGenericSummary.R
@@ -1,8 +1,28 @@
+#' @name OpsSummary
+#' @rdname ContainerS3
+#' @examples
+#' # Summary
+#' range(co)
+#' min(co)
+#' max(co)
+#'
+NULL
+
 #' @export
 Summary.Container <- function(..., na.rm = FALSE)
 {
     get(.Generic)(unlist(lapply(..., unpack)), na.rm = na.rm)
 }
+
+#' @name OpsSummary
+#' @rdname DequeS3
+#' @examples
+#' # Summary
+#' range(d)
+#' min(d)
+#' max(d)
+#'
+NULL
 
 #' @export
 Summary.Deque <- function(..., na.rm = FALSE)
@@ -10,11 +30,31 @@ Summary.Deque <- function(..., na.rm = FALSE)
     get(.Generic)(unlist(lapply(..., unpack)), na.rm = na.rm)
 }
 
+#' @name OpsSummary
+#' @rdname DictS3
+#' @examples
+#' # Summary
+#' range(d)
+#' min(d)
+#' max(d)
+#'
+NULL
+
 #' @export
 Summary.Dict <- function(..., na.rm = FALSE)
 {
     get(.Generic)(unlist(lapply(..., unpack)), na.rm = na.rm)
 }
+
+#' @name OpsSummary
+#' @rdname SetS3
+#' @examples
+#' # Summary
+#' range(s)
+#' min(s)
+#' max(s)
+#'
+NULL
 
 #' @export
 Summary.Set <- function(..., na.rm = FALSE)
