b9b1fb3994ad476fbe086e145cb862e3f9474b2c
Roman Pahl <roman.pahl@gmail.com>
2021-07-31 21:25:34 +0200

    Deprecate delete for dict and cleanup old delete.Dict

--- inst/tinytest/test_delete.R
+++ inst/tinytest/test_delete.R
@@ -21,50 +21,9 @@ expect_true(was_not_touched_on_error)
 
 expect_silent(delete_(co, "a"))
 
-# -----------
-# delete.Dict
-# -----------
 d = dict(a = 1, b = 2, f = mean)
 d2 = clone(d)
-ee(delete(d, "a", "f", "b"), dict())
+ee(delete(d, 1, 2, mean), dict())
 original_was_not_touched = ee(d, d2)
 expect_true(original_was_not_touched)
-
-# args as character vector
-expect_true(is_empty(delete(d, names(d))))
-
-ee(delete_(d, "a", "f", "b"), dict())
-delete_was_done_on_original = ee(d, dict())
-expect_true(delete_was_done_on_original)
-
-d = dict(a = 1, b = 2, f = mean)
-d2 = clone(d)
-expect_error(delete_(d, "a", "x", "y"), "key\\(s\\) not found: x, y")
-was_not_touched_on_error = ee(d, d2)
-expect_true(was_not_touched_on_error)
-
-expect_silent(delete_(d, "a"))
-
-# -----------------
-# delete.dict.table
-# -----------------
-d = dict.table(a = 1, b = 2, f = mean)
-d2 = clone(d)
-expect_true(is_empty(delete(d, "a", 2, "f")))
-ee(d, d2)
-
-# args as character vector
-expect_true(is_empty(delete(d, colnames(d))))
-
-expect_error(delete_(d, "a", 4),
-             "index out of range \\(ncol = 3\\): 4")
-expect_error(delete_(d, "a", "z"),
-             "column\\(s\\) not found: z")
-
-d_was_not_altered = ee(d, d2)
-expect_true(d_was_not_altered)
-
-ee(delete_(d, "b"), d2[, c(1, 3)])
-
-expect_silent(delete_(d, "a"))
 
