852668423c706105aa3d9f515905eef75d8abdfb
Roman Pahl <roman.pahl@gmail.com>
2021-07-31 22:37:27 +0200

    Implement delete_at S3 functions

---
+++ R/delete_at.R
@@ -0,0 +1,182 @@
+#' Delete elements at index
+#'
+#' Search and remove values at given indices, which can be numeric or character
+#' or both. If any given index is invalid, an error is signaled.
+#' @param .x any `R` object.
+#' @param ... indices at which values are to be deleted.
+#' @export
+delete_at <- function(.x, ...) UseMethod("delete_at")
+
+#' @rdname delete_at
+#' @export
+delete_at_ <- function(.x, ...) UseMethod("delete_at_")
+
+
+.has_valid_num_indices.Container <- function(.x, indices)
+{
+    bad_indices = setdiff(indices, seq_len(length(.x)))
+
+    allValid = length(bad_indices) == 0
+    if (allValid)
+        return(TRUE)
+
+    stop("index out of range (length = ", length(.x), "): ", bad_indices[1])
+}
+
+.has_valid_char_indices.Container <- function(.x, indices)
+{
+    bad_names = setdiff(indices, names(.x))
+
+    allValid = length(bad_names) == 0
+    if (allValid)
+        return(TRUE)
+
+    stop("names(s) not found: ", paste0("'", bad_names, "'", collapse = ", "))
+}
+
+
+
+#' @rdname delete_at
+#' @return For `Container`, an object of class `Container` (or one of the
+#' respective derived classes).
+#' @examples
+#'
+#' co = container(a = 1, b = 2, 3)
+#' delete_at(co, "a", "b")          # [3]
+#' delete_at(co, 1:2)               # [3]
+#' delete_at(co, "a", 3)            # [b = 2]
+#' \dontrun{
+#' delete_at(co, 4)                 # index out of range
+#' delete_at(co, "x")               # names(s) not found: 'x'}
+#' @export
+delete_at.Container <- function(.x, ...)
+{
+    (delete_at_(.x$clone(deep = TRUE), ...))
+}
+
+#' @name delete.Container
+#' @rdname ContainerS3
+#' @usage
+#' delete_at(.x, ...)
+#' delete_at_(.x, ...)
+#' @details
+#' * `delete_at(.x, ...)` and `delete_at_(.x, ...)` find and remove values at
+#' given indices. If any given index is invalid, an error is signaled.
+#' @examples
+#'
+#' co = container(a = 1, b = 2, 3)
+#' delete_at(co, "a", "b")          # [3]
+#' delete_at(co, 1:2)               # [3]
+#' delete_at(co, "a", 3)            # [b = 2]
+#' \dontrun{
+#' delete_at(co, 4)                 # index out of range
+#' delete_at(co, "x")               # names(s) not found: 'x'}
+NULL
+
+#' @rdname delete_at
+#' @export
+delete_at_.Container <- function(.x, ...)
+{
+    args = list(...)
+    if (!length(args))
+        return(.x)
+
+    # Numeric indices
+    num_indices = as.integer(unlist(Filter(args, f = is.numeric)))
+    stopifnot(.has_valid_num_indices.Container(.x, num_indices))
+
+    # Character indices
+    char_indices = unlist(Filter(args, f = is.character))
+    stopifnot(.has_valid_char_indices.Container(.x, char_indices))
+
+    num_indices = unique(c(num_indices, match(char_indices, names(.x))))
+
+    lapply(sort(num_indices, decreasing = TRUE), function(i) .x$delete_at(i))
+    invisible(.x)
+}
+
+
+.has_valid_num_indices.dict.table <- function(.x, indices)
+{
+    bad_indices = setdiff(indices, seq_len(ncol(.x)))
+
+    allValid = length(bad_indices) == 0
+    if (allValid)
+        return(TRUE)
+
+    stop("index out of range (ncol = ", ncol(.x), "): ", bad_indices[1])
+}
+
+
+.has_valid_char_indices.dict.table <- function(.x, col_names)
+{
+    bad_names = setdiff(col_names, colnames(.x))
+
+    allValid = length(bad_names) == 0
+    if (allValid)
+        return(TRUE)
+
+    stop("column(s) not found: ", paste0("'", bad_names, "'", collapse = ", "))
+}
+
+
+#' @rdname delete_at
+#' @return For `dict.table`, an object of class `dict.table`.
+#' @examples
+#'
+#' dit = as.dict.table(head(sleep))
+#' dit
+#' delete_at(dit, "ID")
+#' delete_at(dit, "ID", 1)
+#' \dontrun{
+#' delete_at(dit, "foo")   # Column 'foo' not in dict.table}
+#' @export
+delete_at.dict.table <- function(.x, ...)
+{
+    (delete_at_(clone(.x), ...))
+}
+
+
+#' @name delete.dict.table
+#' @rdname dict.table
+#' @usage
+#' delete_at(.x, ...)
+#' delete_at_(.x, ...)
+#' @details
+#' * `delete_at(.x, ...)` and `delete_at_(.x, ...)` find and remove columns either by
+#' name or index (or both). If one or more columns don't exist, an error is signaled.
+#' @examples
+#'
+#' dit = as.dict.table(head(sleep))
+#' dit
+#' delete_at(dit, "ID")
+#' delete_at(dit, "ID", 1)
+#' \dontrun{
+#' delete_at(dit, "foo")   # Column 'foo' not in dict.table}
+NULL
+
+
+#' @rdname delete_at
+#' @export
+delete_at_.dict.table <- function(.x, ...)
+{
+    args = list(...)
+    if (!length(args))
+        return(.x)
+
+    # Indices
+    indices = as.integer(unlist(Filter(args, f = is.numeric)))
+    stopifnot(.has_valid_num_indices.dict.table(.x, indices))
+
+    # Names
+    valid_names = names(.x)[indices]
+    col_names = c(valid_names, unlist(Filter(args, f = is.character)))
+    col_names = unique(col_names)
+    stopifnot(.has_valid_char_indices.dict.table(.x, col_names))
+
+    if (length(col_names))
+        data.table::set(.x, j = col_names, value = NULL)
+
+    invisible(.x)
+}
+
---
+++ inst/tinytest/test_delete_at.R
@@ -0,0 +1,82 @@
+ee = expect_equal
+
+# --------------------
+# delete_at.Container
+# --------------------
+co = container(a = 1, b = 2, f = mean, 3)
+co2 = clone(co)
+ee(delete_at(co), co2)
+ee(delete_at(co, "a"), container(b = 2, f = mean, 3))
+original_was_not_touched = ee(co, co2)
+expect_true(original_was_not_touched)
+
+ee(delete_at(co, "a"), delete_at(co, 1))
+ee(delete_at(co, "b"), delete_at(co, 2))
+ee(delete_at(co, 1:4), container())
+
+ee(delete_at(co, "b", "a", 4:3), container())
+ee(delete_at(co, "a", 1), delete_at(co, 1))
+
+expect_error(delete_at(co, "a", "x"), "names\\(s\\) not found: 'x'")
+was_not_touched_on_error = ee(co, co2)
+expect_true(was_not_touched_on_error)
+
+expect_error(delete_at(co, "a", "x", "y"), "names\\(s\\) not found: 'x', 'y'")
+expect_error(delete_at(co, "a", 1, 5), "index out of range \\(length = 4\\): 5")
+was_not_touched_on_error = ee(co, co2)
+expect_true(was_not_touched_on_error)
+
+ee(delete_at_(co, 1:4), container())
+ee(co, container())
+
+
+# delete_at.Dict
+d = dict(a = 1, b = 2, f = mean)
+d2 = clone(d)
+ee(delete_at(d, "a", "f", "b"), dict())
+original_was_not_touched = ee(d, d2)
+expect_true(original_was_not_touched)
+
+# args as character vector
+expect_true(is_empty(delete_at(d, names(d))))
+
+ee(delete_at_(d, "a", "f", "b"), dict())
+delete_was_done_on_original = ee(d, dict())
+expect_true(delete_was_done_on_original)
+
+d = dict(a = 1, b = 2, f = mean)
+d2 = clone(d)
+expect_error(delete_at_(d, "a", "x", "y"), "names\\(s\\) not found: 'x', 'y'")
+was_not_touched_on_error = ee(d, d2)
+expect_true(was_not_touched_on_error)
+
+
+# -----------------
+# delete.dict.table
+# -----------------
+d = dict.table(a = 1, b = 2, f = mean)
+d2 = clone(d)
+expect_true(is_empty(delete_at(d, "a", 2, "f")))
+ee(d, d2)
+
+ee(delete_at(d, "a", "a"), delete_at(d, "a"))
+ee(delete_at(d, "a", 1), delete_at(d, "a"))
+ee(delete_at(d, 1, "a", 1), delete_at(d, "a"))
+ee(delete_at(d, 1:3), dict.table())
+ee(delete_at(d, 1:3), delete_at(d, 3:1))
+
+# args as character vector
+expect_true(is_empty(delete_at(d, colnames(d))))
+
+expect_error(delete_at_(d, "a", 4),
+             "index out of range \\(ncol = 3\\): 4")
+expect_error(delete_at_(d, "a", "z"),
+             "column\\(s\\) not found: 'z'")
+
+d_was_not_altered = ee(d, d2)
+expect_true(d_was_not_altered)
+
+ee(delete_at_(d, "b"), d2[, c(1, 3)])
+
+expect_silent(delete_at_(d, "a"))
+
