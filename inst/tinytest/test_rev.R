313a16285a457c7fc4bf8253ce5cf2365dce6b1c
Roman Pahl <roman.pahl@gmail.com>
2021-10-11 12:37:00 +0200

    Add unit tests for rev

--- inst/tinytest/test_rev.R
+++ inst/tinytest/test_rev.R
@@ -1,2 +1,15 @@
-ee = expect_equal
+
+# ---------
+# rev.Deque
+# ---------
+v = 1:5
+d = as.deque(v)
+expect_equal(unpack(d), v)
+
+expect_equal(unpack(rev(d)), rev(v))
+d_was_changed = !identical(unpack(d), v)
+expect_false(d_was_changed)
+
+expect_equal(unpack(ref_rev(d)), rev(v))
+expect_equal(unpack(d), rev(v))
 
