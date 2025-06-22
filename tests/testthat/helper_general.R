3cea0526b762ce5c41bbfb2c91e93f8f789fd7f5
Roman Pahl <roman.pahl@gmail.com>
2025-06-22 18:02:49 +0200

    add general helper functions

---
+++ tests/testthat/helper_general.R
@@ -0,0 +1,16 @@
+
+expect_equivalent = function(...) {
+    testthat::expect_equal(..., ignore_attr = TRUE)
+}
+
+expect_error_fixed = function(...) {
+    testthat::expect_error(..., fixed = TRUE)
+}
+
+expect_no_error = function(...) {
+    testthat::expect_error(..., regexp = NA)
+}
+
+expect_no_warning = function(...) {
+    testthat::expect_warning(..., regexp = NA)
+}
